bee3422715c296866f5a6bf4697004a328e91e7e
xDepcio <olek.drwal@gmail.com>
2023-09-09 21:08:46 +0200

    added api endpoint to count stats

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { prisma } from "@/lib/prisma"
 
-export default function Home() {
+export default async function Home() {
+    const stats = await prisma.strapupStats.findFirst({
+        where: {
+            id: 1
+        }
+    })
+
     return (
         <main className="min-h-screen">
+            <p>Templates saved: {stats?.templatesSaved}</p>
+            <p>Templates pasted: {stats?.templatesPasted}</p>
+            <p>Scripts ran: {stats?.scriptsRun}</p>
         </main>
     )
 }
