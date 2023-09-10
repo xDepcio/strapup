8fdd17b3051689ff4abe48247111ecb4fd868147
xDepcio <olek.drwal@gmail.com>
2023-09-10 20:43:20 +0200

    added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navigation from '@/components/Navigation'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
                 <Providers>
                     <Navigation />
                     {children}
+                    <Footer />
                 </Providers>
             </body>
         </html>
---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import { FaGithub, FaNpm } from "react-icons/fa";
+
+export default function Footer() {
+    return (
+        <footer className="border-t-[1px] flex items-center justify-center gap-14 p-4 text-muted-foreground">
+            <p>Created by <Link href={'/'} className="text-foreground underline">xDepcio</Link>, source code avalible on <Link href={'/'} className="text-foreground underline">Github</Link>.</p>
+        </footer>
+    )
+}
