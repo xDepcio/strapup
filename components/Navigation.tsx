f186863503e249be9f23cc9d14ce5fe858e8928c
xDepcio <olek.drwal@gmail.com>
2023-09-11 21:38:31 +0200

    added backdrop blur to main nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,17 +1,14 @@
 import { ProximaSoft } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import { Github, GithubIcon } from "lucide-react";
 import Link from "next/link";
+import { BsGithub } from 'react-icons/bs';
 import { ModeToggle } from "./ui/ModeToggle";
 import { Button } from "./ui/button";
-import { AiFillGithub } from 'react-icons/ai'
-import { BsGithub } from 'react-icons/bs'
-import NavScript from "./NavScript";
 
 export default function Navigation() {
 
     return (
-        <nav id="main-nav" className="w-full flex items-center justify-center sticky transition-all py-4 border-b-[1px] top-0 bg-white z-10 shadow-sm">
+        <nav id="main-nav" className="w-full flex items-center justify-center sticky transition-all py-4 border-b-[1px] top-0 z-10 shadow-sm backdrop-blur-[8px]  bg-white/70">
             <div className="max-w-screen-lg flex items-center justify-between w-full">
                 <div className="flex items-center justify-center">
                     <Link href={'/'} className={cn(ProximaSoft.className, "font-bold text-2xl bg-clip-text text-transparent bg-[rgba(67,_56,_202,_1)]")}>Strapup</Link>
