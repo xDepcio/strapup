247fb9ce4fe22ee76a7b4fcacbfb1bc31fe1fc9e
xDepcio <olek.drwal@gmail.com>
2023-09-11 02:48:25 +0200

    contentlayer setup with styled mdx components

---
+++ app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,25 @@
+import { allDocs } from "@/.contentlayer/generated"
+import { Mdx } from "@/components/mdx-components"
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import { redirect } from 'next/navigation'
+
+const getDocBySlugs = (slugs: string[]) => {
+    const pathFromSlugs = slugs.join('/')
+    const doc = allDocs.find((doc) => doc._raw.flattenedPath === pathFromSlugs)
+    if (!doc) return redirect('/')
+    return doc
+}
+
+export default function DocsPage({ params }: { params: { slug?: string[] } }) {
+    if (!params.slug) return redirect('/docs/instalation')
+
+    const doc = getDocBySlugs(params.slug)
+
+    const MDXContent = useMDXComponent(doc.body.code)
+
+    return (
+        <div className="min-h-screen">
+            <Mdx code={doc?.body.code!} />
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navigation from '@/components/Navigation'
 import Footer from '@/components/Footer'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className={cn(inter.className, "min-h-screen")}>
                 <Providers>
                     <Navigation />
                     {children}
