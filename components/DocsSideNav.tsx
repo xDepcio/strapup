fbbc5aa96beacabbb8cb20f3f2471eeea939012f
xDepcio <olek.drwal@gmail.com>
2023-09-14 18:18:20 +0200

    added darkmode to docs nav

--- components/DocsSideNav.tsx
+++ components/DocsSideNav.tsx
@@ -29,7 +29,7 @@ export default function DocsSideNav() {
                                 {docs.map((doc, j) => {
                                     const isActive = path === `/docs/${doc._raw.flattenedPath}`
 
-                                    return <Link key={j} style={{ contain: "inline-size" }} href={`/docs/${doc._raw.flattenedPath}`} className={cn("ml-0 text-muted-foreground hover:text-indigo-700 transition-all hover:bg-indigo-50 py-1 px-2 rounded-md text-ellipsis whitespace-nowrap overflow-hidden", isActive ? "bg-indigo-50 text-indigo-700" : "")}>{doc.title}</Link>
+                                    return <Link key={j} style={{ contain: "inline-size" }} href={`/docs/${doc._raw.flattenedPath}`} className={cn("ml-0 text-muted-foreground hover:text-indigo-700 dark:hover:text-indigo-600 transition-all hover:bg-indigo-50 py-1 px-2 rounded-md text-ellipsis whitespace-nowrap overflow-hidden dark:hover:bg-zinc-900", isActive ? "bg-indigo-50 dark:bg-zinc-900 dark:text-indigo-600 text-indigo-700" : "")}>{doc.title}</Link>
                                 })}
                             </div>
                         </div>
--- components/PageContentNav.tsx
+++ components/PageContentNav.tsx
@@ -35,13 +35,11 @@ export default function PageContentNav() {
         <div style={{ contain: 'strict' }}>
             <div className="sticky top-[7.04rem]">
                 <h3 className="text-sm font-semibold mb-2">On this page</h3>
-                {/* <h3 className="text-sm font-semibold mb-2">{highlitedHeading?.href}</h3> */}
                 <div className=" flex flex-col gap-1">
                     {linkedHeadings?.map((heading, i) => {
-                        // console.log(heading)
                         return (
                             <div key={i} className=" w-full">
-                                <a href={heading.href} className={cn("text-sm text-muted-foreground hover:text-indigo-700 transition-all hover:bg-indigo-50 py-1 px-2 rounded-md whitespace-nowrap w-full block", heading.href === highlitedHeading?.href ? "text-indigo-700 bg-indigo-50" : "")}>
+                                <a href={heading.href} className={cn("text-sm text-muted-foreground hover:text-indigo-700 transition-all hover:bg-indigo-50 dark:hover:bg-zinc-900 dark:hover:text-indigo-600 py-1 px-2 rounded-md whitespace-nowrap w-full block", heading.href === highlitedHeading?.href ? "text-indigo-700 dark:bg-zinc-900 dark:text-indigo-600 bg-indigo-50" : "")}>
                                     <p className="whitespace-nowrap text-ellipsis overflow-hidden">
                                         {heading.innerText}
                                     </p>
