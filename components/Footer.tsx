aac7e02970e11df81c6e585ed5a3b9cb3c1c5537
xDepcio <olek.drwal@gmail.com>
2023-09-17 16:03:59 +0200

    fixed padding issues with footer on sm devices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub, FaNpm } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer className="border-t-[1px] flex items-center justify-center gap-14 p-4 text-muted-foreground pb-16 sm:pb-4">
+        <footer className="border-t-[1px] flex items-center justify-center gap-14 p-4 text-muted-foreground pb-4">
             <p>Created by <Link href={'https://github.com/xDepcio'} className="text-foreground underline">xDepcio</Link>, source code avalible on <Link href={'https://github.com/xDepcio/strapup'} className="text-foreground underline">Github</Link>.</p>
         </footer>
     )
