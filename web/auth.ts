6babf1ee45e82da69ef0d029549e5bfd0e56e5de
xDepcio <olek.drwal@gmail.com>
2023-11-25 01:18:31 +0100

    added github oAuth authorization with next-auth and persistent storage in postgres. Auth strategy is currently set to session - to change to JWT

--- web/auth.ts
+++ web/auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from 'pg'
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 export const { handlers, auth } = NextAuth({
     providers: [GitHub],
+    adapter: PostgresAdapter(pool)
 })
---
+++ web/db/schema.mjs
@@ -0,0 +1,73 @@
+import { db, sql } from '@vercel/postgres'
+
+/* =========== Tables definitions for next-auth =========== */
+async function createVerificationTokenTable() {
+    await sql`CREATE TABLE IF NOT EXISTS verification_token
+    (
+      identifier TEXT NOT NULL,
+      expires TIMESTAMPTZ NOT NULL,
+      token TEXT NOT NULL,
+
+      PRIMARY KEY (identifier, token)
+    );`
+}
+
+async function createAccountsTable() {
+    await sql`CREATE TABLE IF NOT EXISTS accounts
+    (
+      id SERIAL,
+      "userId" INTEGER NOT NULL,
+      type VARCHAR(255) NOT NULL,
+      provider VARCHAR(255) NOT NULL,
+      "providerAccountId" VARCHAR(255) NOT NULL,
+      refresh_token TEXT,
+      access_token TEXT,
+      expires_at BIGINT,
+      id_token TEXT,
+      scope TEXT,
+      session_state TEXT,
+      token_type TEXT,
+
+      PRIMARY KEY (id)
+    );`
+}
+
+async function createSessionsTable() {
+    await sql`CREATE TABLE IF NOT EXISTS sessions
+    (
+      id SERIAL,
+      "userId" INTEGER NOT NULL,
+      expires TIMESTAMPTZ NOT NULL,
+      "sessionToken" VARCHAR(255) NOT NULL,
+
+      PRIMARY KEY (id)
+    );`
+}
+
+async function createUsersTable() {
+    await sql`CREATE TABLE IF NOT EXISTS users
+    (
+      id SERIAL,
+      name VARCHAR(255),
+      email VARCHAR(255),
+      "emailVerified" TIMESTAMPTZ,
+      image TEXT,
+
+      PRIMARY KEY (id)
+    );`
+}
+/* ========================= */
+
+async function main() {
+    await createVerificationTokenTable()
+    await createAccountsTable()
+    await createSessionsTable()
+    await createUsersTable()
+}
+
+main().catch((err) => {
+    console.error(
+        "An error occured when creating database tables:",
+        err
+    )
+})
