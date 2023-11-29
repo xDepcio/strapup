d7b77b665a14921060d592343878024ed706d373
xDepcio <olek.drwal@gmail.com>
2023-11-29 02:38:41 +0100

    basic fiber setup

---
+++ files-server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	fmt.Println("Hello, World!")
+
+	log.Fatal(app.Listen(":3000"))
+}
