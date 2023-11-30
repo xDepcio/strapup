27ed38b36c715a2baa727ce1bbc03b77defeb7f5
xDepcio <olek.drwal@gmail.com>
2023-11-30 01:37:50 +0100

    added folders for middlewares, handlers and main router

--- files-server/internal/router/router.go
+++ files-server/internal/router/router.go
@@ -1,1 +1,11 @@
 package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api", logger.New())
+
+}
