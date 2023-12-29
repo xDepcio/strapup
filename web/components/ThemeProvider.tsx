b3532731e9099cb2d4b083c534cd5a9ab7c3457d
xDepcio <olek.drwal@gmail.com>
2023-12-29 23:08:07 +0100

    updated default theme to dark

--- web/components/ThemeProvider.tsx
+++ web/components/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemeProvider } from "next-themes"
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
     return (
-        <NextThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <NextThemeProvider attribute="data-theme" defaultTheme="light" enableSystem>
 
                 {children}
