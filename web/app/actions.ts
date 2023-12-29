7887eecc2887be803dc284d09c3f2434026d74b5
xDepcio <olek.drwal@gmail.com>
2023-12-30 00:38:57 +0100

    added basic searching logic

--- web/app/actions.ts
+++ web/app/actions.ts
@@ -1,1 +1,36 @@
 'use server'
+// import { sql } from "@vercel/postgres"
+
+// type FetchTemplatesScriptsParams = {
+//     searchInput: string
+//     searchScripts: boolean
+//     searchTemplates: boolean
+// }
+// export async function fetchTemplatesScript({ searchInput, searchScripts, searchTemplates }: FetchTemplatesScriptsParams) {
+//     const splittedSearchInput = searchInput.split(' ')
+//     const sqlRegex = splittedSearchInput.join('|')
+//     console.log({ sqlRegex })
+
+//     let scripts = []
+//     if (searchScripts) {
+//         console.log('before quary')
+//         const { command, fields, oid, rowCount, rows } = await sql`SELECT name, tags, stars FROM TEMPLATES
+//             WHERE public IS TRUE
+//             AND (
+//                 LOWER(name) LIKE '%(${sqlRegex})%'
+//                 OR tags SIMILAR TO '%(${sqlRegex})%'
+//             )
+//             ORDER BY stars DESC
+//         ;`
+
+//         console.log({ command, fields, oid, rowCount, rows })
+//     }
+// }
+
+
+export async function serverFn() {
+    'use server'
+
+    console.log('server side code');
+
+}
--- web/components/Search.tsx
+++ web/components/Search.tsx
@@ -4,20 +4,40 @@ import {
     DialogContent,
     DialogTrigger
 } from "@/components/ui/dialog";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import { CiHashtag } from "react-icons/ci";
 import { FaSearch } from "react-icons/fa";
 import { Skeleton } from "./ui/skeleton";
 import { Switch } from "./ui/switch";
 import { Label } from "./ui/label";
+import { serverFn } from "@/app/actions";
+
 
 export default function Search() {
     const searchRef = useRef<HTMLButtonElement>(null)
+    const [dbSearchTimeout, setDbSearchTimeout] = useState<NodeJS.Timeout | null>(null)
+    const [searchScripts, setSearchScripts] = useState<boolean>(true)
+    const [searchTemplates, setSearchTemplates] = useState<boolean>(false)
+
     useHotkeys('ctrl+k', (e) => {
         e.preventDefault()
         searchRef.current?.click()
     })
+
+    function handleSearchInput(e: React.ChangeEvent<HTMLInputElement>) {
+        const timeout = setTimeout(async () => {
+            console.log('searching...')
+            await serverFn()
+            // await fetchTemplatesScript({
+            //     searchInput: e.target.value,
+            //     searchScripts: searchScripts,
+            //     searchTemplates: searchTemplates
+            // })
+        }, 500)
+        clearTimeout(dbSearchTimeout as NodeJS.Timeout)
+        setDbSearchTimeout(timeout)
+    }
 
     return (
         <Dialog>
@@ -35,16 +55,16 @@ export default function Search() {
             <DialogContent className="max-w-[624px] top-1/3 p-0 py-2 border border-zinc-200 dark:border-zinc-900">
                 <div className="">
                     <div className="border-b border-zinc-200 dark:border-zinc-900">
-                        <input className="text-lg px-4 py-2 outline-none w-full dark:bg-zinc-950" placeholder="Search templates and scripts..." />
+                        <input onChange={handleSearchInput} className="text-lg px-4 py-2 outline-none w-full dark:bg-zinc-950" placeholder="Search templates and scripts..." />
                     </div>
                     <div className="p-4">
                         <div className="mb-4 flex items-center gap-4 text-muted-foreground">
                             <Label className="flex items-center gap-2">
-                                <Switch defaultChecked />
+                                <Switch defaultChecked onClick={() => setSearchScripts(!searchScripts)} />
                                 <p>Search scripts</p>
                             </Label>
                             <Label className="flex items-center gap-2">
-                                <Switch />
+                                <Switch onClick={() => setSearchTemplates(!searchTemplates)} />
                                 <p>Search templates</p>
                             </Label>
                         </div>
