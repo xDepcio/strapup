772910cabe75888fa3aa0ab7f79a8a91cf2607f6
xDepcio <olek.drwal@gmail.com>
2024-01-05 02:04:39 +0100

    query for all template info

--- web/app/templates/[slug]/page.tsx
+++ web/app/templates/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { DBQuery } from '@/db/db'
-import { DbTemplte } from '@/db/types'
+import { DbTemplte, DbUser } from '@/db/types'
 import { unescapeName } from '@/lib/utils'
 import { sql } from '@vercel/postgres'
 import { redirect } from 'next/navigation'
@@ -7,9 +7,11 @@ import { redirect } from 'next/navigation'
 export default async function TemplatePage({ params }: { params: { slug: string } }) {
     const templateName = decodeURIComponent(unescapeName(params.slug))
     console.log(templateName)
-    const { rows, rowCount } = await DBQuery<DbTemplte>('SELECT name, tags, stars FROM templates WHERE name = $1 AND public IS TRUE', [templateName])
-
-    console.log(rows, rowCount)
+    const { rows, rowCount } = await DBQuery<Pick<DbTemplte, "name" | "tags" | "stars"> & Pick<DbUser, "image" | "login" | "github_id">>(`
+        SELECT t.name, t.tags, t.stars, u.login, u.image, u.github_id FROM templates t
+        JOIN users u ON t.owner_id = u.id
+        WHERE t.name = $1 AND t.public IS TRUE
+    `, [templateName])
 
     if (rowCount === 0) {
         return <div>404</div>
