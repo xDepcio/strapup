42e7ef507ae1ed65913df7c81f624d784f0fe168
xDepcio <olek.drwal@gmail.com>
2024-01-06 18:55:49 +0100

    Refactor SearchPage component to display formatted data

--- web/app/search/page.tsx
+++ web/app/search/page.tsx
@@ -3,32 +3,42 @@
 import { SearchContext } from "@/components/Providers";
 import { SearchFilters } from "@/components/SearchFiltersForm"
 import { Skeleton } from "@/components/ui/skeleton";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 
 export default function SearchPage() {
     const { data, isLoading } = useContext(SearchContext)
+    const formattedData = useMemo(() => {
+        return [
+            ...data.scripts.map(e => ({ ...e, type: 'script' })),
+            ...data.templates.map(e => ({ ...e, type: 'template' })),
+        ].sort((a, b) => a.stars - b.stars)
+    }, [data])
+
     return (
-        <div className="mx-auto min-h-screen grid grid-cols-[20%_auto] max-w-screen-lg mt-10">
+        <div className="mx-auto min-h-screen grid grid-cols-[20%_auto] max-w-screen-lg mt-10 gap-4">
             <SearchFilters />
-            <div>
+            <div className="flex gap-2 h-fit flex-col">
                 {isLoading ? (
                     <>
+                        <Skeleton className="w-full h-20" />
+                        <Skeleton className="w-full h-20" />
+                        <Skeleton className="w-full h-20" />
                         <Skeleton className="w-full h-20" />
                     </>
                 ) : (
                     <>
-                        {data.scripts.map((script) => (
-                            <div key={script.id} className="flex items-center space-x-4">
-                                <p className="text-sm text-muted-foreground">Script</p>
-                                <p className="text-sm text-muted-foreground">{script.name}</p>
-                            </div>
-                        ))}
-                        {data.templates.map((template) => (
-                            <div key={template.id} className="flex items-center space-x-4">
-                                <p className="text-sm text-muted-foreground">Template</p>
-                                <p className="text-sm text-muted-foreground">{template.name}</p>
-                            </div>
-                        ))}
+                        {formattedData.length === 0 ? (
+                            <div>no results</div>
+                        ) : (
+                            <>
+                                {formattedData.map((entry) => (
+                                    <div key={entry.id} className="flex items-center space-x-4">
+                                        <p className="text-sm text-muted-foreground">Script</p>
+                                        <p className="text-sm text-muted-foreground">{entry.name}</p>
+                                    </div>
+                                ))}
+                            </>
+                        )}
                     </>
                 )}
             </div>
