e94bb24423c23cabade2a566e918bd212baa8a59
xDepcio <olek.drwal@gmail.com>
2024-01-08 19:45:41 +0100

    added more data to scripts and templates seeds

--- web/db/seeds/seed-scripts.mjs
+++ web/db/seeds/seed-scripts.mjs
@@ -5,7 +5,28 @@ async function createDummyScripts() {
         ('@xDepcio/script1', true, 1, 'nextjs react typescript tailwind'),
         ('@xDepcio/script2', false, 1, 'express sequelize postgres socketio'),
         ('@xDepcio/script3', true, 1, 'python flask sql'),
-        ('@xDepcio/script4', false, 1, 'sql express')
+        ('@xDepcio/script4', false, 1, 'sql express'),
+        ('@xDepcio/script5', true, 1, 'javascript vue nodejs'),
+        ('@xDepcio/script6', true, 1, 'angular typescript rxjs'),
+        ('@xDepcio/script7', true, 1, 'react native redux'),
+        ('@xDepcio/script8', true, 1, 'php laravel mysql'),
+        ('@xDepcio/script9', true, 1, 'ruby rails postgresql'),
+        ('@xDepcio/script10', true, 1, 'java spring boot hibernate'),
+        ('@xDepcio/script11', true, 1, 'c# .net entityframework'),
+        ('@xDepcio/script12', true, 1, 'go gin gorm'),
+        ('@xDepcio/script13', true, 1, 'kotlin spring react'),
+        ('@xDepcio/script14', true, 1, 'swift vapor postgresql'),
+        ('@xDepcio/script15', true, 1, 'docker kubernetes cloud'),
+        ('@xDepcio/script16', true, 1, 'graphql apollo server'),
+        ('@xDepcio/script17', true, 1, 'html css javascript bootstrap'),
+        ('@xDepcio/script18', true, 1, 'typescript webpack babel'),
+        ('@xDepcio/script19', true, 1, 'rust actix-web wasm'),
+        ('@xDepcio/script20', true, 1, 'elixir phoenix liveview'),
+        ('@xDepcio/script21', true, 1, 'perl catalyst dbix'),
+        ('@xDepcio/script22', true, 1, 'nodejs express mongodb'),
+        ('@xDepcio/script23', true, 1, 'svelte sapper tailwindcss'),
+        ('@xDepcio/script24', true, 1, 'haskell yesod postgresql'),
+        ('@xDepcio/script25', true, 1, 'clojure ring datomic')
     ;`
 }
 
--- web/db/seeds/seed-templates.mjs
+++ web/db/seeds/seed-templates.mjs
@@ -5,7 +5,28 @@ async function createDummyTemplates() {
         ('@xDepcio/template1', true, 1, 'nextjs react typescript tailwind'),
         ('@xDepcio/template2', false, 1, 'express sequelize postgres socketio'),
         ('@xDepcio/template3', true, 1, 'python flask sql'),
-        ('@xDepcio/template4', false, 1, 'express sql postgres')
+        ('@xDepcio/template4', false, 1, 'express sql postgres'),
+        ('@xDepcio/template5', true, 1, 'vue laravel mysql'),
+        ('@xDepcio/template6', true, 1, 'django react postgresql'),
+        ('@xDepcio/template7', true, 1, 'spring angular mongodb'),
+        ('@xDepcio/template8', true, 1, 'ruby rails sqlite'),
+        ('@xDepcio/template9', true, 1, 'nodejs express mongo'),
+        ('@xDepcio/template10', true, 1, 'go echo sqlserver'),
+        ('@xDepcio/template11', true, 1, 'php symfony mariadb'),
+        ('@xDepcio/template12', true, 1, 'java vertx cassandra'),
+        ('@xDepcio/template13', true, 1, 'rust actix graphql'),
+        ('@xDepcio/template14', true, 1, 'kotlin ktor redis'),
+        ('@xDepcio/template15', true, 1, 'elixir phoenix arangodb'),
+        ('@xDepcio/template16', true, 1, 'swift vapor dynamodb'),
+        ('@xDepcio/template17', true, 1, 'csharp aspnet neo4j'),
+        ('@xDepcio/template18', true, 1, 'perl dancer elasticsearch'),
+        ('@xDepcio/template19', true, 1, 'c++ cpprestsdk orientdb'),
+        ('@xDepcio/template20', true, 1, 'javascript nextjs firebase'),
+        ('@xDepcio/template21', true, 1, 'typescript nestjs rethinkdb'),
+        ('@xDepcio/template22', true, 1, 'svelte kit couchdb'),
+        ('@xDepcio/template23', true, 1, 'fsharp giraffe aerospike'),
+        ('@xDepcio/template24', true, 1, 'haskell yesod etcd'),
+        ('@xDepcio/template25', true, 1, 'clojure luminus rocksdb')
     ;`
 }
 
