cc0b5be8d4a921d2d851d51e456b1b8d31eb2c19
xDepcio <olek.drwal@gmail.com>
2024-01-08 23:31:58 +0100

    added sign in flow

---
+++ web/components/LoginUserClient.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { HTMLAttributes } from "react"
+import { Button } from "./ui/button"
+import { BsGithub } from "react-icons/bs"
+import { signIn } from "next-auth/react"
+
+interface LoginUserClientProps extends HTMLAttributes<HTMLButtonElement>, React.ComponentProps<typeof Button> {
+}
+
+export default function LoginUserClient({ ...props }: LoginUserClientProps) {
+    return (
+        <Button {...props} onClick={() => signIn('github')} >
+            <p>Sign In</p>
+            <BsGithub className="text-[1rem]" />
+        </Button>
+    )
+}
--- web/components/Navigation.tsx
+++ web/components/Navigation.tsx
@@ -5,8 +5,11 @@ import { BsGithub } from 'react-icons/bs';
 import { ModeToggle } from "./ui/ModeToggle";
 import { Button } from "./ui/button";
 import Search from "./Search";
+import { getServerSession } from "next-auth";
+import LoginUserClient from "./LoginUserClient";
 
-export default function Navigation() {
+export default async function Navigation() {
+    const user = await getServerSession()
 
     return (
         <nav id="main-nav" className="w-full flex items-center justify-center sticky transition-all py-4 border-b-[1px] top-0 z-10 shadow-sm backdrop-blur-[8px] bg-white/70 dark:bg-zinc-950/70">
@@ -22,13 +25,17 @@ export default function Navigation() {
                         </Button>
                         {/* <Link href={'/docs'}></Link> */}
                     </Link>
-                    <Link href={"https://github.com/xDepcio/strapup-mono"}>
-                        <Button variant={"ghost"} className="flex gap-2 items-center justify-center">
-                            {/* <Button>sign in</Button> */}
-                            <p>Profile</p>
-                            <BsGithub className="text-[1rem]" />
-                        </Button>
-                    </Link>
+                    {user ? (
+                        <Link href={user ? "/user" : '/api/auth/signin'}>
+                            <Button variant={"ghost"} className="flex gap-2 items-center justify-center">
+                                {/* <Button>sign in</Button> */}
+                                <p>Profile</p>
+                                <BsGithub className="text-[1rem]" />
+                            </Button>
+                        </Link>
+                    ) : (
+                        <LoginUserClient className="flex gap-2 items-center justify-center" variant={'ghost'} />
+                    )}
                     {/* <div className="flex items-center justify-center">
                     </div> */}
                     <ModeToggle />
