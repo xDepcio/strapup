ab479961645e2b3a09176f245a442cf3c086ff9b
kjurczyn <01169180@pw.edu.pl>
2024-01-09 01:03:01 +0100

    added template downloading functionality

--- cli/src/commandsHandlers.ts
+++ cli/src/commandsHandlers.ts
@@ -87,6 +87,58 @@ interface PasteOptions {
     destinationRelativePath: string
 }
 
+//use templateEntry?
+interface folderStruct{
+    Name: string,
+    IsDir: boolean,
+    Children: folderStruct[]
+}
+
+async function constructTemplate(my_struct: folderStruct, my_relative_path: string, my_absolute_path: string)
+{
+    
+    if(my_struct.IsDir)
+    {
+        const cur_absolute_path = path.normalize(`${my_absolute_path}${my_struct.Name}/`)
+        const cur_relative_path = path.normalize(`${my_relative_path}${my_struct.Name}/`)
+        try    
+        {
+            fs.mkdirSync(cur_absolute_path, {recursive: true})
+        }
+        catch (err)
+        {
+            throw new Error(`Failed to create folder ${cur_absolute_path}. Error: ${err}`)
+        }
+        if(my_struct.Children)
+        {
+            my_struct.Children.forEach((child: folderStruct) =>
+            {
+                constructTemplate(child, cur_relative_path, cur_absolute_path)
+            })
+        }
+    }
+    else
+    {
+        const cur_absolute_path = path.normalize(`${my_absolute_path}${my_struct.Name}`)
+        const cur_relative_path = path.normalize(`${my_relative_path}${my_struct.Name}`)
+        const curFileContentResponse = await fetch(`${GO_BACKEND_URL}/api/templates/file?name=${cur_relative_path}`,{
+            method: 'GET',
+            headers: {
+                'Authorization': `${loadSettings().githubToken}`
+            }
+        })
+        
+        if(!curFileContentResponse.ok)
+        {
+            throw new Error(`Failed to download file ${cur_relative_path}. Error: ${curFileContentResponse.statusText}`)
+        }
+        const curFileContent: string = await curFileContentResponse.text() 
+
+        fs.writeFileSync(cur_absolute_path, curFileContent)
+    }
+    return
+}
+    
 export async function paste({ templateName, destinationRelativePath }: PasteOptions) {
     const templatePath = path.normalize(`${TEMPLATES_PATH()}/${templateName}`)
     const destinationAbsolutePath = path.normalize(`${WORK_DIR}/${destinationRelativePath}`)
@@ -103,16 +155,22 @@ export async function paste({ templateName, destinationRelativePath }: PasteOpti
         }) 
         
         if (!templateStructureResponse.ok) {
-            p.log.error(`Failed to download template. Error: ${templateStructureResponse.statusText}`)
+            p.log.error(`Failed to download template structure. Error: ${templateStructureResponse.statusText}`)
             return
         }
-        const templateStructureJson = await templateStructureResponse.json()
+        const templateStructureJson: folderStruct = await templateStructureResponse.json()        
 
-        //TODO iterate over (potentially) nested jsons 
-        templateStructureJson.forEach()
+        try
+        {
+            constructTemplate(templateStructureJson, ``, templatePath)
+        }
+        catch(err)
+        {
+            p.log.warn(`Failed to download and construct template. ${err}`)
+            fs.rmSync(templatePath, {recursive: true})
+            return
+        }
 
-        // p.log.error(`Template ${templateName} does not exist. Aborting.`)
-        return
     }
 
     if (!fs.existsSync(destinationAbsolutePath)) {
