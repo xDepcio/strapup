9c3eda72cd3931ee588a4dd7d890744d400c0e1c
xDepcio <olek.drwal@gmail.com>
2024-01-10 02:30:58 +0100

    added starred templates and script to user nav

--- web/app/user/layout.tsx
+++ web/app/user/layout.tsx
@@ -2,32 +2,41 @@
 
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
-import { FaCode } from "react-icons/fa";
+import { FaCode, FaStar } from "react-icons/fa";
 import { MdOutlineSdStorage, MdStorage } from "react-icons/md";
 import { FaSignOutAlt } from "react-icons/fa";
 import Logout from "@/components/Logut";
 import Link from "next/link";
+import { FaRegStar } from "react-icons/fa6";
 
 export default function UserPage({ children }: { children: React.ReactNode }) {
     const pathname = usePathname()
 
     return (
         <div className="min-h-screen mx-auto max-w-screen-lg grid grid-cols-[20%_auto] mt-16 mb-16">
-            <div className="flex flex-col gap-4 text-muted-foreground">
-                <Link href={'/user/templates'} className={cn("flex gap-2 items-center cursor-pointer hover:text-indigo-600 transition-all", pathname === '/user/templates' ? 'text-indigo-600 font-medium' : '')}>
+            <div className="flex flex-col gap-2 text-muted-foreground">
+                <Link href={'/user/templates'} className={cn("py-2 flex gap-2 items-center cursor-pointer hover:text-indigo-600 transition-all", pathname === '/user/templates' ? 'text-indigo-600 font-medium' : '')}>
                     <p>My Templates</p>
                     <MdOutlineSdStorage />
                 </Link>
-                <Link href={'/user/scripts'} className={cn("flex gap-2 items-center cursor-pointer hover:text-indigo-600 transition-all", pathname === '/user/scripts' ? 'text-indigo-600 font-medium' : '')}>
+                <Link href={'/user/scripts'} className={cn("py-2 flex gap-2 items-center cursor-pointer hover:text-indigo-600 transition-all", pathname === '/user/scripts' ? 'text-indigo-600 font-medium' : '')}>
                     <p>My Scripts</p>
                     <FaCode />
                 </Link>
-                {/* <div className={"flex gap-2 items-center cursor-pointer hover:text-red-500 transition-all"}> */}
-                <Logout className="flex gap-2 items-center cursor-pointer hover:text-red-500 transition-all">
+                <Link href={'/user/starred-templates'} className={cn("py-2 flex gap-2 items-center cursor-pointer hover:text-indigo-600 transition-all", pathname === '/user/starred-templates' ? 'text-indigo-600 font-medium' : '')}>
+                    <FaRegStar />
+                    <p>Starred Templates</p>
+                    <MdOutlineSdStorage />
+                </Link>
+                <Link href={'/user/starred-scripts'} className={cn("py-2 flex gap-2 items-center cursor-pointer hover:text-indigo-600 transition-all", pathname === '/user/starred-scripts' ? 'text-indigo-600 font-medium' : '')}>
+                    <FaRegStar />
+                    <p>Starred Scripts</p>
+                    <FaCode />
+                </Link>
+                <Logout className="py-2 flex gap-2 items-center cursor-pointer hover:text-red-500 transition-all">
                     <p>Logout</p>
                     <FaSignOutAlt />
                 </Logout>
-                {/* </div> */}
             </div>
             {children}
         </div>
