c56a203111bb9557fbcf7005ecfdf4c459e9b1a2
xDepcio <olek.drwal@gmail.com>
2024-06-25 01:05:49 +0200

    removed junk dupliacted files

--- prisma/migrations/20230909183754_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "StrapupStats" (
-    "id" SERIAL NOT NULL,
-    "templatesSaved" INTEGER NOT NULL,
-    "templatesPasted" INTEGER NOT NULL,
-    "scriptsRun" INTEGER NOT NULL,
-
-    CONSTRAINT "StrapupStats_pkey" PRIMARY KEY ("id")
-);
--- src/premade-templates/python_project/python_project/main.py
+++
@@ -1,2 +0,0 @@
-def fun(x):
-    return x+2
