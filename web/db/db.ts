bbfdd4ae7f84647f2c32b1c26535b87676b840a8
xDepcio <olek.drwal@gmail.com>
2024-06-25 16:50:11 +0200

    updated web .env file and package json to disable SSL connections to db

--- web/db/db.ts
+++ web/db/db.ts
@@ -7,7 +7,7 @@ import { Client, QueryResult, QueryResultRow } from "pg";
 
 export async function DBQuery<T extends QueryResultRow>(query: string, params: any[] = []): Promise<QueryResult<T>> {
     const dbClient = new Client({
-        connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+        connectionString: process.env.POSTGRES_URL,
     })
 
     await dbClient.connect()
