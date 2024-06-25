4911584c3c6f46d6331da4ea2bc366d7cdcc6725
xDepcio <olek.drwal@gmail.com>
2024-06-25 17:19:18 +0200

    changed scheme scripts to work with db hosted on any provider

---
+++ web/db/schemas/next-auth-schema.ts
@@ -0,0 +1,84 @@
+import { DBQuery } from '../db'
+
+/* =========== Tables definitions for next-auth =========== */
+async function createVerificationTokenTable() {
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS verification_token
+        (
+        identifier TEXT NOT NULL,
+        expires TIMESTAMPTZ NOT NULL,
+        token TEXT NOT NULL,
+
+        PRIMARY KEY (identifier, token)
+        );`
+    )
+}
+
+async function createAccountsTable() {
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS accounts
+        (
+        id SERIAL,
+        "userId" INTEGER NOT NULL,
+        type VARCHAR(255) NOT NULL,
+        provider VARCHAR(255) NOT NULL,
+        "providerAccountId" VARCHAR(255) NOT NULL,
+        refresh_token TEXT,
+        access_token TEXT,
+        expires_at BIGINT,
+        id_token TEXT,
+        scope TEXT,
+        session_state TEXT,
+        token_type TEXT,
+
+        PRIMARY KEY (id)
+        );`
+    )
+}
+
+async function createSessionsTable() {
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS sessions
+        (
+        id SERIAL,
+        "userId" INTEGER NOT NULL,
+        expires TIMESTAMPTZ NOT NULL,
+        "sessionToken" VARCHAR(255) NOT NULL,
+
+        PRIMARY KEY (id)
+        );`
+    )
+}
+
+async function createUsersTable() {
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS users
+        (
+        id SERIAL,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        "emailVerified" TIMESTAMPTZ,
+        image TEXT,
+        login VARCHAR(255),
+        github_id INTEGER,
+
+        PRIMARY KEY (id)
+        );`
+    )
+}
+/* ========================= */
+
+async function main() {
+    await createVerificationTokenTable()
+    await createAccountsTable()
+    await createSessionsTable()
+    await createUsersTable()
+    console.log("Tables created successfully")
+}
+
+main().catch((err) => {
+    console.error(
+        "An error occured when creating database tables:",
+        err
+    )
+})
--- web/db/schemas/schema.ts
+++ web/db/schemas/schema.ts
@@ -1,9 +1,10 @@
-import { sql } from '@vercel/postgres'
+import { DBQuery } from '../db'
 
 
 async function createTemplatesTable() {
-    await sql`CREATE TABLE IF NOT EXISTS templates
-    (
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS templates
+        (
         id SERIAL,
         name VARCHAR(255) NOT NULL,
         description VARCHAR(1024),
@@ -13,12 +14,14 @@ async function createTemplatesTable() {
         synced BOOLEAN DEFAULT FALSE,
 
         PRIMARY KEY (id)
-    );`
+        );`
+    )
 }
 
 async function createScriptsTable() {
-    await sql`CREATE TABLE IF NOT EXISTS scripts
-    (
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS scripts
+        (
         id SERIAL,
         name VARCHAR(255) NOT NULL,
         description VARCHAR(1024),
@@ -28,27 +31,32 @@ async function createScriptsTable() {
         synced BOOLEAN DEFAULT FALSE,
 
         PRIMARY KEY (id)
-    );`
+        );`
+    )
 }
 
 async function createUserTemplateStars() {
-    await sql`CREATE TABLE IF NOT EXISTS user_template_stars
-    (
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS user_template_stars
+        (
         user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
         template_id INTEGER NOT NULL REFERENCES templates(id) ON DELETE CASCADE,
 
         PRIMARY KEY (user_id, template_id)
-    );`
+        );`
+    )
 }
 
 async function createUserScriptStars() {
-    await sql`CREATE TABLE IF NOT EXISTS user_script_stars
-    (
+    await DBQuery(
+        `CREATE TABLE IF NOT EXISTS user_script_stars
+        (
         user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
         script_id INTEGER NOT NULL REFERENCES scripts(id) ON DELETE CASCADE,
 
         PRIMARY KEY (user_id, script_id)
-    );`
+        );`
+    )
 }
 
 async function main() {
