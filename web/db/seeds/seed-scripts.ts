0941a075e1f0fad38b1f42a9213fba76c5038346
xDepcio <olek.drwal@gmail.com>
2024-08-01 12:46:08 +0200

    renamed stored files to match stored data in db

--- web/db/seeds/seed-scripts.ts
+++ web/db/seeds/seed-scripts.ts
@@ -2,7 +2,7 @@ import { DBQuery } from "../db";
 
 export async function createDummyScripts() {
     const queryString = `INSERT INTO scripts (id, name, public, owner_id, tags) VALUES
-        (99999999, '@xDepcio/Create-Next-ShadcnUI', true, 99999999, 'nextjs react typescript tailwind')
+        (99999999, '@zlacoreczka/Create-Next-ShadcnUI', true, 99999999, 'nextjs react typescript tailwind')
     ;`
 
     await DBQuery(queryString)
