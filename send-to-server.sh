7263de89b663793e9cbed3df57d8c5734cc2fbe0
xDepcio <olek.drwal@gmail.com>
2024-08-01 17:37:16 +0200

    Updated send to server script

--- send-to-server.sh
+++ send-to-server.sh
@@ -1,7 +1,10 @@
 tar --exclude='*/node_modules' --exclude='*/.git' --exclude='*/.contentlayer' --exclude='*/.next' -czvf archive_name.tar.gz *
-scp archive_name.tar.gz adrwal@192.168.0.24:~/strapup
+scp archive_name.tar.gz adrwal@192.168.0.24:~/
 
 ssh adrwal@192.168.0.24 << 'ENDSSH'
+rm -rf ~/strapup
+mkdir ~/strapup
+mv archive_name.tar.gz ~/strapup
 cd ~/strapup
 tar -xzvf archive_name.tar.gz
 rm archive_name.tar.gz
